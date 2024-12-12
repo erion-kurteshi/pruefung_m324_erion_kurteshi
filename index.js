14ff2a67ae141f33433843315c458590de04fec2
Erion Kurteshi <Erion_Kurteshi@sluz.ch>
2024-12-12 09:33:20 +0100

    Revert "formatter test"
    This reverts commit 232cb8c74000efc92cd99a21fc53e5b7c46295d7.

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 import chalk from 'chalk';
 
 const myVariable = 42; // Fixed 'prefer-const' by using const
-if (
-  myVariable ===
-  '42'
-) {
-  // Fixed 'eqeqeq' by using strict equality (===)
-  console.log(
-    chalk.blue(
-      'It works',
-    ),
-  );
+if (myVariable === '42') { // Fixed 'eqeqeq' by using strict equality (===)
+    console.log(chalk.blue('It works'));
 }
