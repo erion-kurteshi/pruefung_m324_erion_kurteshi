bea5189f9a40dc213f39c3c6c4d739edb298219d
Erion Kurteshi <Erion_Kurteshi@sluz.ch>
2024-12-12 09:27:13 +0100

    linter test

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-import chalk from 'chalk'
-console.log(chalk.blue('It works'))
+import chalk from 'chalk';
+
+let myUnusedVariable = 42; // Trigger 'no-unused-vars' and 'prefer-const'
+if (myUnusedVariable == '42') { // Trigger 'eqeqeq'
+    console.log(chalk.blue('It works'));
+}
